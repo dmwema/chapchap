e1212a572bf17fb83449e880d6462c51ee243228
dmwema <danielmwemakapwe@gmail.com>
2022-11-01 23:50:24 +0100

    add homepage cards

---
+++ lib/res/components/home_card.dart
@@ -0,0 +1,46 @@
+import 'package:chapchap/res/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeCard extends StatelessWidget {
+  final double h;
+  final Color color;
+  final IconData icon;
+  final title;
+
+  const HomeCard({
+    required this.h,
+    required this.color,
+    required this.icon,
+    required this.title
+  });
+
+   @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        child: Container(
+            width: (MediaQuery.of(context).size.width - 60)  / 2,
+            height: h,
+            decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(20)
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(icon, size: 40, color: Colors.white,),
+                  const SizedBox(height: 5,),
+                  Text(title, style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            )
+        ),
+      );
+  }
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:chapchap/res/app_colors.dart';
+import 'package:chapchap/res/components/home_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -12,8 +14,45 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Text("aze"),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text("Salut"),
+            Text("Al-Bakr Sanogo"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    HomeCard(
+                      h: 150,
+                      color: AppColors.primaryColor,
+                      icon: Icons.payment_rounded,
+                      title: "Envoi d'argent",
+                    ),
+                    SizedBox(height: 20,),
+                    HomeCard(h: 90, color: AppColors.darkRed2, icon: Icons.bar_chart, title: "Bénéficiaires")
+                  ],
+                ),
+                SizedBox(width: 20,),
+                Column(
+                  children: [
+                    HomeCard(h: 90, color: AppColors.darkRed, icon: Icons.compare_arrows_rounded, title: "Taux de change"),
+                    SizedBox(height: 20,),
+                    HomeCard(
+                      h: 150,
+                      color: AppColors.marronRed,
+                      icon: Icons.history_rounded,
+                      title: "Historique des transferts",
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       )
     );
   }
